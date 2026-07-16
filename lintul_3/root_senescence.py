73c491f3ebcd0c98b286b4c7dfca3f270dd52ccd
bergh026 <herman.berghuijs@wur.nl>
2026-07-16 16:52:48 +0200

    Documentation added.

--- lintul_3/root_senescence.py
+++ lintul_3/root_senescence.py
@@ -1,7 +1,40 @@
+# -*- coding: utf-8 -*-
+# Herman Berghuijs (herman.berghuijs@wur.nl)
+# July 2026
+
 from pcse.traitlets import Float
 from pcse.base import SimulationObject, ParamTemplate, StatesTemplate, RatesTemplate
 
 class RootSenescence(SimulationObject):
+    """"
+    Class to simulate root senescence
+
+    Simulates root senescence and the corresponding nitrogen loss. Root senescence starts once
+    a particular development stage is exceeded. Once that happens, root dry matter dies exponentially
+    assuming a constant relative growth rate. Due to root dry matter loss, root nitrogen is lost too.
+    It is assumed that the nitrogen concentration in dying leaf dry  matter equals its residual
+    nitrogen concentration.
+
+    *Simulation parameters*
+
+    ==============  ==============================================  ======  ===========================
+     Name            Description                                    Type     Unit
+    ==============  ==============================================  ======  ===========================
+    DVSDR           Development stage above which root senescence
+                    starts                                          SCr     -
+    RNFRT           Residual nitrogen concentration of roots        SCr     g N g-1 DM
+    ==============  ==============================================  ======  ===========================
+
+    *Rate variables*
+
+    ==============  ==============================================  ======  ==============================
+     Name            Description                                    Pbl     Unit
+    ==============  ==============================================  ======  ==============================
+    RDRT            Death rate of root dry matter                   SCr     g DM m-2 d-1
+    RDRTN           Nitrogen loss rate due to root senescence       SCr     g DM m-2 d-1
+    ==============  ==============================================  ======  ==============================
+    """
+
     class Parameters(ParamTemplate):
         DVSDR = Float()
         RNFRT = Float()
