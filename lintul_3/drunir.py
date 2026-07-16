bb1c9cc9d23215428333d4732fc8edfa566644f6
bergh026 <herman.berghuijs@wur.nl>
2026-07-16 11:31:47 +0200

    Documentation added.

--- lintul_3/drunir.py
+++ lintul_3/drunir.py
@@ -9,6 +9,52 @@ cm_to_mm = 1e1
 m_to_mm = 1e3
 
 class Drunir(SimulationObject):
+    """
+    Class to simulate the rates of drainage, irrigation, and surface run-off.
+
+    Simulate the rates of drainage, irrigation, and surface run-off. The amount of water that is
+    removed by drainage during one time step is the minimum of two potential amounts. The first
+    potential amount is the difference between the actual amount of water in the rooted soil and the
+    amount of water at field capacity in the rooted soil. The second potential amount is a maximum
+    amount that can be removed during one time step. If the amount of water in the rooted soil is
+    still above the amount of field capacity, the amount of water that is removed by surface-runoff
+    equals the amount required to bring the amount of water in the soil at the amount at field capcity.
+    Irrigation rates are calculated as a fraction of the amount of water that is required to bring the
+    soil moisture content to field capacity after one timestep.
+
+    *Simulation parameters*
+
+    ==============  ==============================================  ======  ==============================
+     Name            Description                                    Type     Unit
+    ==============  ==============================================  ======  ==============================
+    DRATE           Maximum drainage rate of the rooted soil.       SCr     mm3 water m-2 rooted soil d-1
+    IRRIGF          Fraction of the amount of water that is
+                    required to bring the soil moisture content
+                    to field capacity that is applied by
+                    irrigation.                                     SCr     -
+    WCFC            Soil moistore content at field capacity.        SCr     mm3 water m-3 rooted soil
+    WCST            Soil moisture content at saturation.            SCr     mm3 water m-3 rooted soil
+    WMFAC           Switch to determine whether (=1) or not (=0)
+                    the simulated system is flooded rice.           SCr     -
+    ==============  ==============================================  ======  ==============================
+
+    *Rate variables*
+    ==============  ==============================================  ======  ==============================
+     Name            Description                                    Pbl     Unit
+    ==============  ==============================================  ======  ==============================
+    RIRRIG          Irrigation rate                                 Y       mm3 water m-2 rooted soil d-1
+    RDRAIN          Drainage rate                                   Y       mm3 water m-2 rooted soil d-1
+    RRUNOFF         Surface run-off-rate                            Y       mm3 water m-2 rooted soil d-1
+    ==============  ==============================================  ======  ==============================
+
+    *Auxiliary variables*
+    ==============  ==============================================  ======  ==============================
+     Name            Description                                    Pbl     Unit
+    ==============  ==============================================  ======  ==============================
+    WAST            Amount of water in rooted soil at saturation    N       mm3 water m-2 rooted soil
+    ==============  ==============================================  ======  ==============================
+
+    """
     class Parameters(ParamTemplate):
         DRATE = Float()
         IRRIGF = Float()
